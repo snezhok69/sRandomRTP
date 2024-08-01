7d288211cf0034975be932fda6c8422b715d3131
snezhok69_ <150421263+snezhok69@users.noreply.github.com>
2024-08-01 22:42:43 +0300

    update

--- src/main/java/org/sRandomRTP/GetYGet/GetSafeYCoordinate.java
+++ src/main/java/org/sRandomRTP/GetYGet/GetSafeYCoordinate.java
@@ -1,12 +1,21 @@
 package org.sRandomRTP.GetYGet;
 
+import io.papermc.lib.PaperLib;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
 import org.sRandomRTP.DifferentMethods.LoggerUtility;
 
+import java.util.concurrent.CompletableFuture;
+
 public class GetSafeYCoordinate {
+
+    public static CompletableFuture<Integer> getSafeYCoordinateAsync(World world, int x, int z) {
+        return PaperLib.getChunkAtAsync(world, x >> 4, z >> 4, true)
+                .thenApply(chunk -> getSafeYCoordinate(world, x, z));
+    }
+
     public static int getSafeYCoordinate(World world, int x, int z) {
         try {
             for (int y = world.getMaxHeight() - 1; y > 0; y--) {
@@ -37,24 +46,26 @@ public class GetSafeYCoordinate {
         }
     }
 
-    public static CoordinateWithBiome getSafeYCoordinateWithAirCheck(World world, int x, int z) {
-        try {
-            int y = getSafeYCoordinate(world, x, z);
-            if (y == -1) return null;
+    public static CompletableFuture<CoordinateWithBiome> getSafeYCoordinateWithAirCheckAsync(World world, int x, int z) {
+        return PaperLib.getChunkAtAsync(world, x >> 4, z >> 4, true)
+                .thenApply(chunk -> {
+                    int y = getSafeYCoordinate(world, x, z);
+                    if (y == -1) return null;
 
-            Block blockAbove = world.getBlockAt(x, y + 1, z);
-            Block blockTwoAbove = world.getBlockAt(x, y + 2, z);
+                    Block blockAbove = world.getBlockAt(x, y + 1, z);
+                    Block blockTwoAbove = world.getBlockAt(x, y + 2, z);
 
-            if (blockAbove.getType() == Material.AIR && blockTwoAbove.getType() == Material.AIR) {
-                Biome biome = world.getBiome(x, y, z);
-                return new CoordinateWithBiome(y, biome);
-            }
-        } catch (Throwable e) {
-            StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-            String callingClassName = stackTrace[2].getClassName();
-            LoggerUtility.loggerUtility(callingClassName, e);
-        }
-        return null;
+                    if (blockAbove.getType() == Material.AIR && blockTwoAbove.getType() == Material.AIR) {
+                        Biome biome = world.getBiome(x, y, z);
+                        return new CoordinateWithBiome(y, biome);
+                    }
+                    return null;
+                });
+    }
+
+    public static CompletableFuture<Integer> getSafeYCoordinateFromBottomAsync(World world, int x, int z) {
+        return PaperLib.getChunkAtAsync(world, x >> 4, z >> 4, true)
+                .thenApply(chunk -> getSafeYCoordinateFromBottom(world, x, z));
     }
 
     public static int getSafeYCoordinateFromBottom(World world, int x, int z) {
