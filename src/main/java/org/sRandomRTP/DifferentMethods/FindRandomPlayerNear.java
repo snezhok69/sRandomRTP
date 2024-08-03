dfa2e8801a3fe5a796846a326c37332bf94a0649
snezhok69 <maximnn2704@icloud.com>
2024-08-03 15:43:07 +0300

    pre-update folia

---
+++ src/main/java/org/sRandomRTP/DifferentMethods/FindRandomPlayerNear.java
@@ -0,0 +1,31 @@
+package org.sRandomRTP.DifferentMethods;
+
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class FindRandomPlayerNear {
+    public static Player findRandomPlayer(Player player, List<Player> allPlayers) {
+        try {
+            List<Player> sameWorldPlayers = allPlayers.stream()
+                    .filter(p -> p.getWorld().equals(player.getWorld()) && !p.equals(player))
+                    .collect(Collectors.toList());
+
+            if (sameWorldPlayers.isEmpty()) {
+                return null;
+            }
+
+            // Рандомизация из списка игроков в том же мире
+            Random random = new Random();
+            return sameWorldPlayers.get(random.nextInt(sameWorldPlayers.size()));
+        } catch (Throwable e) {
+            // Логирование ошибок
+            StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+            String callingClassName = stackTrace[2].getClassName();
+            LoggerUtility.loggerUtility(callingClassName, e);
+        }
+        return null;
+    }
+}
